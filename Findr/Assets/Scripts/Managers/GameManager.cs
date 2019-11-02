e42d7a980a56631f1ab07154bd832290f03f328e
Dev <dprasad@knights.ucf.edu>
2019-11-02 00:33:52 -0400

    Initial Commit
    yey lobby screen here.

---
+++ Findr/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager main;
+
+
+
+
+
+    private void Awake()
+    {
+        if(main != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        main = this;
+        DontDestroyOnLoad(this);
+
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Findr/Assets/Scripts/TitleScreenScripts/TS_Input.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TS_Input : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("You touched me!");
+            SceneManager.LoadScene(1);
+        }
+    }
+}
