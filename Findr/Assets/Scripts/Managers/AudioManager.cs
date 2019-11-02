e3524f0b4b7691ab25ee5a7fb949167304bb62a1
Thomas Pring <pringthomas4@gmail.com>
2019-11-02 18:15:27 -0400

    Added UIManager and AudioManager

---
+++ Findr/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Static Members
+
+    public static AudioManager main;
+
+    #endregion
+
+    #region RunTime Members
+
+    public AudioMixer Mixer;
+
+    public AudioSource FxSource;
+    public AudioSource MusicSource;
+
+    public AudioClip LobbyMusic;
+    public AudioClip GameMusic;
+
+    private string SceneName = "";
+
+    #endregion
+
+    #region MonoBehavior
+
+    private void Awake()
+    {
+        if (main != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        main = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
